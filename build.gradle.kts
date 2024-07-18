83e62dc6eb6cc59f233e27e54b7ac76efb777b39
itsnotoger <19309683+itsnotoger@users.noreply.github.com>
2024-07-18 20:45:31 +0200

    Update rest of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 group = "org.pircbotx"
-version = "2.1.3-twitchify"
+version = "2.5.0-twitchify"
 
 tasks {
     compileTestJava {
@@ -43,9 +43,9 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("info.unterrainer.java.tools:nullannotations:0.3")
-    implementation("commons-codec:commons-codec:1.10")
+    implementation("commons-codec:commons-codec:1.13")
 
-    testImplementation("org.testng:testng:6.9.10")
+    testImplementation("org.testng:testng:7.7.0")
     testImplementation("org.mockito:mockito-core:2.0.40-beta")
-    testImplementation("ch.qos.logback:logback-classic:1.1.3")
+    testImplementation("ch.qos.logback:logback-classic:1.4.14")
 }
