7afe926b5a4e30ad42b9443c82e4d3abeab675d6
itsnotoger <19309683+itsnotoger@users.noreply.github.com>
2024-07-19 01:23:40 +0200

    Add extra java module info plugin
    * to ensure transitive dependencies work with this plugin - not needed by this project itself

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     `java-library`
     id("io.freefair.lombok") version "8.6"
     `maven-publish`
+    id("org.gradlex.extra-java-module-info") version "1.8"
 }
 
 repositories {
     mavenCentral()
 }
 
-group = "org.pircbotx"
-version = "2.5.0-twitchify"
+group = "com.github.itsnotoger"
+version = "2.5.1-twitchify"
 
 tasks {
     compileTestJava {
@@ -19,7 +20,7 @@ tasks {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11));
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
     withSourcesJar()
 //    withJavadocJar() TODO needs a lot of fixing
@@ -38,11 +39,10 @@ publishing {
 }
 
 dependencies {
-    api("com.google.guava:guava:32.0.0-android")
+    api("com.google.guava:guava:33.2.1-jre")
 
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.slf4j:slf4j-api")
-    implementation("info.unterrainer.java.tools:nullannotations:0.3")
     implementation("commons-codec:commons-codec:1.13")
 
     implementation("org.jetbrains:annotations")
@@ -59,6 +59,23 @@ dependencies {
         implementation("org.jetbrains:annotations") {
             version { require("24.1.0") }
             because("modularized jar needed")
+        }
+    }
+}
+
+extraJavaModuleInfo {
+    module("com.google.guava:listenablefuture", "guava.listenablefuture")
+    module("com.google.code.findbugs:jsr305", "javax.annotation") {
+        exports("javax.annotation")
+        exports("javax.annotation.concurrent")
+        exports("javax.annotation.meta")
+    }
+}
+
+configurations {
+    listOf("testCompileClasspath", "testRuntimeClasspath").forEach { configName ->
+        named(configName).configure {
+            attributes.attribute(Attribute.of("javaModule", Boolean::class.javaObjectType), false)
         }
     }
 }
